15727cd18d1ff504b793f29454f2eb5d659adb92
nonicknamelala <ujsxb@163.com>
2021-12-03 11:42:07 +0800

    update git protocol

--- diy-part2-k2p.sh
+++ diy-part2-k2p.sh
@@ -13,5 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone git@github.com:MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0.git package/mtk
+git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0.git package/mtk
 
